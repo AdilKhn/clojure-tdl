ee4b7ba58d87286fa0e4217bebeb894c372ef985
Adil Khan <adil.khan@outlook.com>
2016-12-19 18:10:04 -0500

    Add Module to Write Function Tests

---
+++ spec/functions_spec.clj
@@ -0,0 +1,8 @@
+(ns myspecljproj.core-spec
+  (:require [speclj.core :refer :all]
+            [myspecljproj.core :refer :all]
+            [myspecljproj.functions :refer :all]))
+
+(describe "simple function"
+  (it "returns a simple string"
+  (should= "simple" (simple-func))))
---
+++ src/myspecljproj/functions.clj
@@ -0,0 +1,4 @@
+(ns myspecljproj.functions)
+
+(defn simple-func
+  ([] "simple"))
