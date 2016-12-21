0a5b822804ac8c81f9200cd27ae4252a982d6ad8
Adil Khan <adil.khan@outlook.com>
2016-12-20 20:56:23 -0500

    Adds Two Parameter Function

--- spec/myspecljproj/functions_spec.clj
+++ spec/myspecljproj/functions_spec.clj
@@ -9,3 +9,7 @@
 (describe "Func with 1 param"
   (it "uses one parameter"
   (should= "You passed homer" (one-param-func "homer"))))
+
+(describe "func with 2 params"
+   (it "uses two parameters"
+  (should= "clo|jure" (two-param-func "clo" "jure"))))
--- src/myspecljproj/functions.clj
+++ src/myspecljproj/functions.clj
@@ -2,6 +2,12 @@
 
 (defn simple-func
   ([] "simple"))
+
 (defn one-param-func 
   ([a] (str "You passed " a)))
-  
+
+(defn two-param-func
+  "this is a two parameter function"
+  ([a b] (str a "|" b)))
+
+
